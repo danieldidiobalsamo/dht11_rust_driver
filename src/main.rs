0b688d2fc30dccd6474a65dc15b24f28899bcb73
danieldidiobalsamo <79797812+danieldidiobalsamo@users.noreply.github.com>
2025-08-08 14:26:25 +0200

    feat: DHT11 driver

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,28 @@
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
-use esp_hal::timer::timg::TimerGroup;
+use esp_hal::{
+    gpio::{Level, Output, OutputConfig},
+    timer::timg::TimerGroup,
+};
+use weather_station::print_measurements;
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
-#[embassy_executor::task]
-async fn embassy_task() {
-    // Task Initializations
-    loop {
-        // Task Loop Code
-        esp_println::println!("Print from an embassy task");
-        Timer::after(Duration::from_millis(1_000)).await;
-    }
-}
-
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    esp_println::println!("Init!");
     let peripherals = esp_hal::init(esp_hal::Config::default());
 
-    // Initalize embassy executor
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timg0.timer0);
 
-    spawner.spawn(embassy_task()).unwrap();
+    let pin = Output::new(peripherals.GPIO0, Level::High, OutputConfig::default());
+
+    spawner.spawn(print_measurements(pin.into_flex())).unwrap();
 
     loop {
-        // Main loop code
-        esp_println::println!("Print from the main task");
-        Timer::after(Duration::from_millis(5_000)).await;
+        Timer::after(Duration::from_millis(2_000)).await;
     }
 }
